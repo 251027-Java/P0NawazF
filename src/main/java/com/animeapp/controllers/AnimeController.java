edf1b9835a914bb2c80dc64926a506d3b0d8c453
FahadNawazCs <fahadnawaz.cs99@gmail.com>
2025-11-18 22:09:47 -0600

    modify package path

---
+++ src/main/java/com/animeapp/controllers/AnimeController.java
@@ -0,0 +1,51 @@
+package com.animeapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.animeapp.model.User;
+import com.animeapp.service.AnimeService;
+import com.animeapp.service.UserService;
+
+import java.util.Objects;
+
+@RestController
+public class AnimeController {
+    private UserService userService;
+    private AnimeService animeService;
+
+    @Autowired
+    public AnimeController(AnimeService animeService, UserService userService) {
+        this.animeService = animeService;
+        this.userService = userService;
+
+    }
+
+    @PostMapping("/register")
+    ResponseEntity<User> registerUser(@RequestBody User request) {
+        if (userService.doesUsernameExist(request.getUsername())) {
+            return ResponseEntity.status(409).body(request);
+        }
+        User registeredUser = userService.registerUser(request.getUsername(), request.getPassword());
+        if (Objects.isNull(registeredUser)) {
+            return ResponseEntity.status(400).body(request);
+        }
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    ResponseEntity<User> login(@RequestBody User request) {
+
+        User loggedInAccount = userService.loginUser(request);
+
+        if (Objects.isNull(loggedInAccount)) {
+            return ResponseEntity.status(401).body(null);
+        }
+
+        return ResponseEntity.ok(loggedInAccount);
+    }
+
+}
