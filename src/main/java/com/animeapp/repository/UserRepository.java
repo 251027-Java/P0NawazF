b11041e39f4b53423b3f39e35a2363355931713b
FahadNawazCs <fahadnawaz.cs99@gmail.com>
2025-11-20 22:37:35 -0600

    rename method

--- src/main/java/com/animeapp/repository/UserRepository.java
+++ src/main/java/com/animeapp/repository/UserRepository.java
@@ -12,6 +12,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsernameAndPassword(String username, String password);
 
-    boolean existsUserById(Integer id);
+    boolean existsUserByUserId(Integer userId);
 
 }
