1d4751687e8d974fbbc4e19852cbdfa77f588460
FahadNawazCs <fahadnawaz.cs99@gmail.com>
2025-11-20 22:39:53 -0600

    add error handling to UserService

--- src/main/java/com/animeapp/service/UserService.java
+++ src/main/java/com/animeapp/service/UserService.java
@@ -1,5 +1,6 @@
 package com.animeapp.service;
 
+import com.animeapp.exceptions.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +11,28 @@ import com.animeapp.repository.UserRepository;
 public class UserService {
     private UserRepository userRepository;
 
+    private static final Boolean isLoggedIn = Boolean.FALSE;
+
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User registerUser(String username, String password) {
+    public User registerUser(String username, String password) throws UserException{
         if (username.isBlank() || password.isBlank() || password.length() < 8) {
-            return null;
+            throw new UserException("Incorrect username or password.");
         }
-        User savedUser = userRepository.save(new User(username, password));
-        return savedUser;
+        return userRepository.save(new User(username, password));
     }
 
     public boolean doesUsernameExist(String username) {
         return userRepository.existsUserByUsername(username);
     }
 
-    public User loginUser(User user) {
+    public User loginUser(User user) throws UserException{
+        if(isLoggedIn){
+            throw new UserException("Please log out first before trying to log in again.");
+        }
         String username = user.getUsername();
         String password = user.getPassword();
 
